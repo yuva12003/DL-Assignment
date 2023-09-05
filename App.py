389207c19d328b90279a8ac742e4336eeae76a70
yuva12003 <115409039+yuva12003@users.noreply.github.com>
2023-09-06 02:30:20 +0530

    Update App.py

--- App.py
+++ App.py
@@ -41,14 +41,14 @@ st.write("If you would not like to upload an image, you can use the sample image
 sample_img_choice = st.button("Use Sample Image")
 
 if sample_img_choice:
-    image1 = Image.open("card.jpg")
+    image1 = Image.open("R_6.jpg")
     image1=image.smart_resize(image1,(224, 224))
     img_array = image.img_to_array(image1)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array/255.0
     predictions = model.predict(img_array)
     label=labels[np.argmax(predictions)]
-    image1 = Image.open("dummy.jpg")
+    image1 = Image.open("R_6.jpg")
     st.image(image1, caption="Uploaded Image", use_column_width=True)    
     st.markdown(
         f"<h2 style='text-align: center;'>{label}</h2>",
